f5c28a6a14d718d17fe67c348908dda8c0800ede
bayraaa11234 <greenbayraaa@gmail.com>
2025-09-09 17:38:45 +0800

    1111111111

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Emotion_face from "./pages/Emotion_face/Emotion_face";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/emotion_face" element={<Emotion_face />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Emotion_face/Emotion_face.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useRef, useState } from "react";
+import * as faceapi from "face-api.js";
+
+export default function EmotionCamera() {
+  const videoRef = useRef();
+  const canvasRef = useRef();
+  const [emotion, setEmotion] = useState("");
+
+  useEffect(() => {
+    const loadModels = async () => {
+      await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+      await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+      startVideo();
+    };
+
+    const startVideo = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+      videoRef.current.srcObject = stream;
+    };
+
+    loadModels();
+  }, []);
+
+  const handleVideoOnPlay = () => {
+    const displaySize = {
+      width: videoRef.current.width,
+      height: videoRef.current.height,
+    };
+    faceapi.matchDimensions(canvasRef.current, displaySize);
+
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+        .withFaceExpressions();
+
+      const resized = faceapi.resizeResults(detections, displaySize);
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+      ctx.drawImage(
+        videoRef.current,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+
+      if (resized.length > 0) {
+        const expressions = resized[0].expressions;
+        const maxEmotion = Object.keys(expressions).reduce((a, b) =>
+          expressions[a] > expressions[b] ? a : b
+        );
+        setEmotion(maxEmotion);
+      }
+    }, 500);
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <video
+        ref={videoRef}
+        width="640"
+        height="480"
+        autoPlay
+        muted
+        onPlay={handleVideoOnPlay}
+      />
+      <canvas
+        ref={canvasRef}
+        width="640"
+        height="480"
+        style={{ position: "absolute", top: 0, left: 0 }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: 10,
+          left: 10,
+          fontSize: "30px",
+          color: "red",
+        }}
+      >
+        {emotion}
+      </div>
+    </div>
+  );
+}
