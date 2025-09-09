f63d0804a90bb769e7f868c8a28f9ac0df9afe67
bayraaa11234 <greenbayraaa@gmail.com>
2025-09-09 17:47:40 +0800

    link added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ import Camera_identify from "./pages/Camera_identify/Camera_identify";
 function App() {
   return (
     <>
+      <div>
+        <h1 style={{ textAlign: "center" }}>AI Test Applications</h1>
+        <div>
+          {" "}
+          <a href="/" style={{ margin: "10px" }}>
+            Home
+          </a>
+        </div>
+        <div>
+          <a href="/emotion_face" style={{ margin: "10px" }}>
+            Emotion Face
+          </a>
+        </div>
+        <div>
+          <a href="/camera_identify" style={{ margin: "10px" }}>
+            Camera Identify
+          </a>
+        </div>
+      </div>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
