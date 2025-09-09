963f54385d67c0639e317c1a29d1a2df5669b9fa
bayraaa11234 <greenbayraaa@gmail.com>
2025-09-09 17:43:02 +0800

    some changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Emotion_face from "./pages/Emotion_face/Emotion_face";
+import Camera_identify from "./pages/Camera_identify/Camera_identify";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/emotion_face" element={<Emotion_face />} />
+          <Route path="/camera_identify" element={<Camera_identify />} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/Camera_identify/Camera_identify.jsx
@@ -0,0 +1,82 @@
+// ObjectDetection.js
+import React, { useRef, useEffect } from "react";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs";
+
+export default function Camera_identify() {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const setupCamera = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 640, height: 480 },
+        audio: false,
+      });
+      videoRef.current.srcObject = stream;
+      return new Promise((resolve) => {
+        videoRef.current.onloadedmetadata = () => resolve(videoRef.current);
+      });
+    };
+
+    const runCoco = async () => {
+      await setupCamera();
+      videoRef.current.play();
+
+      const model = await cocoSsd.load();
+
+      const detectFrame = async () => {
+        const predictions = await model.detect(videoRef.current);
+
+        const ctx = canvasRef.current.getContext("2d");
+        ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+        ctx.drawImage(
+          videoRef.current,
+          0,
+          0,
+          canvasRef.current.width,
+          canvasRef.current.height
+        );
+
+        predictions.forEach((pred) => {
+          const [x, y, width, height] = pred.bbox;
+          ctx.strokeStyle = "red";
+          ctx.lineWidth = 2;
+          ctx.strokeRect(x, y, width, height);
+          ctx.font = "18px Arial";
+          ctx.fillStyle = "red";
+          ctx.fillText(
+            `${pred.class} ${Math.round(pred.score * 100)}%`,
+            x,
+            y > 20 ? y - 5 : y + 20
+          );
+        });
+
+        requestAnimationFrame(detectFrame);
+      };
+
+      detectFrame();
+    };
+
+    runCoco();
+  }, []);
+
+  return (
+    <div style={{ position: "relative", width: 640, height: 480 }}>
+      <video
+        ref={videoRef}
+        width="640"
+        height="480"
+        autoPlay
+        muted
+        style={{ position: "absolute", top: 0, left: 0 }}
+      />
+      <canvas
+        ref={canvasRef}
+        width="640"
+        height="480"
+        style={{ position: "absolute", top: 0, left: 0 }}
+      />
+    </div>
+  );
+}
